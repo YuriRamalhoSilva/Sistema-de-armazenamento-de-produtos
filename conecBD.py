671bcd31362e41338c7b3ace5d7f51b55065a733
YuriRamalhoSilva <yuriramalhosilva67@gmail.com>
2024-10-14 19:05:08 -0300

    continuaçao classe conecBD

--- conecBD.py
+++ conecBD.py
@@ -6,15 +6,54 @@ conexao = mysql.connector.connect(
       password= '',
       database= 'BDcrudpy')
 cursor = conexao.cursor()
+
+
 class conecBD:
    
-    def inserir():
-        cursor.execute("")
-        pass
+    def inserir_User(email,senha):
+        valores = (email,senha)
+        
+        sql = 'Insert into Table_Users (email,senha) values (%s,%s)'
+        
+        cursor.execute(sql,valores)
+        
+        if conexao.is_connected():
+         print("Conectou")
+        else:
+         print("Não Conectou")
 
-    def deletar():
-        cursor.execute("")
-        pass
+        conexao.commit()
+        cursor.close()
+        conexao.close()     
+        
+
+    def deletar_User(email):
+    
+     try:
+            valores =(email,)
+            sql = ("DELETE FROM Table_Users WHERE email = %s")
+        
+            cursor.execute(sql,valores)
+
+            if conexao.is_connected():
+                print("Conectou")
+            else:
+                print("Não Conectou")
+
+            conexao.commit()
+
+            if cursor.rowcount > 0:
+                print(f"User {email} deletado com sucesso do banco de dados!")
+            else:
+                print(f"User {email} nao foi encontrado no banco de dados!")
+
+     except mysql.connector.Error as err:
+                print(f"Erro ao deletar usuário: {err}")
+     finally:
+    
+        cursor.close()
+        conexao.close()
+        
 
     def alterar():
         cursor.execute("")
@@ -23,4 +62,7 @@ class conecBD:
     def select():
         cursor.execute("")
         pass
+
+    
+    deletar_User("email2@gmail.com")
 
